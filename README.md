a3de3c2475fae2025cfa9162369de4e0929f7747
Whido-IT <yuri.scabio@whido.com.br>
2026-07-09 13:19:02 -0300

    Script para retomar o acesso ao NAS após a atualização para o Win11

---
+++ README.md
@@ -0,0 +1,9 @@
+<h3>Nota Importante</h3>
+
+<p>Algumas das máquinas perderam acesso ao NAS (Netbios) após a atualização 
+para o Windows 11. Criei esse script, testei na minha máquina e o acesso foi 
+restaurado.</p>
+
+<p>Se acontecer com você também, por favor teste esse script e me informe do
+resultado.</p>
+
---
+++ reset-nas.ps1
@@ -0,0 +1,47 @@
+# Executar como Administrador
+
+# Verifica privilégios
+$admin = ([Security.Principal.WindowsPrincipal] `
+    [Security.Principal.WindowsIdentity]::GetCurrent()
+).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $admin) {
+    Write-Host "Execute este script como Administrador."
+    exit
+}
+
+
+# Permitir logons Guest SMB
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\LanmanWorkstation" -Force | Out-Null
+
+New-ItemProperty `
+    -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\LanmanWorkstation" `
+    -Name "AllowInsecureGuestAuth" `
+    -Value 1 `
+    -PropertyType DWord `
+    -Force | Out-Null
+
+# Serviços
+
+$services = @(
+    "lmhosts",
+    "LanmanWorkstation",
+    "FDResPub",
+    "fdPHost"
+)
+
+foreach ($service in $services) {
+    try {
+        Set-Service $service -StartupType Automatic
+        Start-Service $service -ErrorAction SilentlyContinue
+    }
+    catch {}
+}
+
+# Limpa caches
+
+ipconfig /flushdns | Out-Null
+nbtstat -R | Out-Null
+nbtstat -RR | Out-Null
+
+shutdown -r -t 0
